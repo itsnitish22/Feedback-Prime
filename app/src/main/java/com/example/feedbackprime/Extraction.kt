dcac33429b727a1c812994783383b66438e4298c
itsnitish22 <nitish.sharma1186@gmail.com>
2021-10-17 01:27:56 +0530

    made a function to send the post request and get the access token from the API

--- app/src/main/java/com/example/feedbackprime/Extraction.kt
+++ app/src/main/java/com/example/feedbackprime/Extraction.kt
@@ -1,38 +1,47 @@
 package com.example.feedbackprime
 
-import android.app.ProgressDialog
-import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.android.volley.RequestQueue
-import com.android.volley.Response
-import com.android.volley.VolleyLog.TAG
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.example.feedbackprime.databinding.ActivityExtractionBinding
+import org.json.JSONObject
 
 class Extraction : AppCompatActivity() {
-    private val url = intent.extras?.getString("url")
+//    private val url = intent.extras?.getString("url")
+    private var accessToken: String = ""
+    lateinit var binding: ActivityExtractionBinding
+
+    private val newUrl = "https://api.symbl.ai/oauth2/token:generate"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_extraction)
+        binding = ActivityExtractionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val url = intent.extras?.getString("url")
+
+        sendAppId()
     }
 
-    var volleyRequestQueue: RequestQueue? = null
-    var dialog: ProgressDialog? = null
-    val serverAPIURL: String? = url
-
-    fun sendAppId() {
-        val volleyRequestQueue = Volley.newRequestQueue(this)
-        val dialog = ProgressDialog.show(this, "", "Please wait...", true)
-        val parameters: MutableMap<String, String> = HashMap()
-
+    private fun sendAppId() {
+        val parameters = JSONObject()
         parameters.put("type", "application")
         parameters.put("appId", "316d68796574616370626830674c713457436241795068556a7277523853626e")
         parameters.put(
             "appSecret",
             "57546179597542416a4e324d45542d6c543176394a48344d65564557613068434f726f426c6f627744787365746c304d654b6843377147334b75736b34574277"
         )
+
+        val req = JsonObjectRequest(Request.Method.POST, newUrl, parameters,
+            {
+                accessToken = it.getString("expiresIn").toString()
+            }, {
+                Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+            })
+        MySingleton.getInstance(this).addToRequestQueue(req)
+
+        binding.access.text = accessToken
     }
 }
